7c117f2cb9cd8943d7f6d2fa6ea98e2cdd23cfa1
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2019-11-13 21:53:49 -0300

    Prueba de tiempos

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,3 +1,4 @@
+import time
 import itertools
 def comparacionesDimension(dimension):
     l=[list(e) for e in itertools.product(*[[">","<="] for x in range(dimension)])]
@@ -5,6 +6,7 @@ def comparacionesDimension(dimension):
     return l
 
 """
+    n*(2^r)*r
     No se pueden tener puntos duplicados en la lista de puntos al momento de crear
 """
 def crearArbol(puntos,dimension):
@@ -37,6 +39,9 @@ def crearArbol(puntos,dimension):
         resultado.append(crearArbol(a,dimension))
     return (medianas,resultado)
 
+"""
+    (2^r)*r
+"""
 def buscar(arbol,punto):
     if arbol == None:
         return False
@@ -62,21 +67,56 @@ def buscar(arbol,punto):
 
 import random
 
-random.seed(30)
-puntos = []
+for k in [5,10,15,20]:
+    random.seed(30)
+    print("Start "+str(k))
+    start = time.perf_counter()
+    puntos = []
+    for n in [10]:#[100000,500000,1000000]:
+        for i in range(n):
+            while True:
+                p=[]
+                for d in range(k):
+                    p.append(random.randint(0,10000))
+                if p not in puntos:
+                    puntos.append(p)
+                    break
+    elapsed = time.perf_counter()
+    print("--- Generacion puntos %s segundos ---" % (elapsed-start))
+    start = time.perf_counter()
+    arbol=crearArbol(puntos,k)
+    elapsed = time.perf_counter()
+    print("--- Creacion arbol %s segundos ---" % (elapsed-start))
 
-dimension = 2
-for i in range(10000):
-    while True:
-        p=[]
-        for d in range(dimension):
-            p.append(random.randint(0,40000))
-        if p not in puntos:
-            puntos.append(p)
-            break
-arbol = crearArbol(puntos,dimension)
+    tiemposBusquedaAcierto=[]
+    tiemposBusquedaIncierta=[]
+    print("Inicio busqueda aciertos")
+    for p in puntos:
+        start = time.perf_counter()
+        buscar(arbol,p)
+        elapsed = time.perf_counter()
+        tiemposBusquedaAcierto.append(elapsed-start)
+    i=0
+    print("--- Busqueda aciertos %s segundos ---" % (sum(tiemposBusquedaAcierto)/len(tiemposBusquedaAcierto)))
+    random.seed(70)
+    print("Inicio busqueda inciertos")
+    while i<100:
+        while True:
+            p=[]
+            for d in range(k):
+                p.append(random.randint(0,10000))
+            print(p)
+            print(p in puntos)
+            if p not in puntos:
+                start = time.perf_counter()
+                buscar(arbol,p)
+                elapsed = time.perf_counter()
+                tiemposBusquedaIncierta.append(elapsed-start)
+                break
+        i+=1
+    print("--- Busqueda inciertos %s segundos ---" % (sum(tiemposBusquedaIncierta)/len(tiemposBusquedaIncierta)))
+    print("--- Busqueda total %s segundos ---" % ((sum(tiemposBusquedaAcierto)+sum(tiemposBusquedaIncierta))/(len(tiemposBusquedaIncierta)+len(tiemposBusquedaAcierto))))
 
-print(arbol)
-print("======BUSQUEDA=======")
 
-print(buscar(arbol,[16,3]))
+#print("======BUSQUEDA=======")
+#print(buscar(arbol,[4760,10479]))
